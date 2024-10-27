b11e2eb340a747dadd87f77e38d9c209f709f444
ogahsergesergehoo <sergeleo14@yahoo.fr>
2024-10-27 22:03:43 +0000

    rf

--- afriqconsulting/settings.py
+++ afriqconsulting/settings.py
@@ -177,7 +177,7 @@ TINYMCE_DEFAULT_CONFIG = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_URL = '/afrique-consulting/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
