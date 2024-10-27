77202afbe83c6df902eb11f7e9e8c6896184dc8e
ogahsergesergehoo <sergeleo14@yahoo.fr>
2024-10-27 22:39:27 +0000

    as

--- afriqconsulting/settings.py
+++ afriqconsulting/settings.py
@@ -179,7 +179,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'staticfiles')
 
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
