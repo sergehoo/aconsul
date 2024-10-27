70601da8108e5b7005b3dea2a1d7ef709fe9e014
ogahsergesergehoo <sergeleo14@yahoo.fr>
2024-10-27 22:53:52 +0000

    tr

--- afriqconsulting/settings.py
+++ afriqconsulting/settings.py
@@ -178,9 +178,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_FINDERS = [
